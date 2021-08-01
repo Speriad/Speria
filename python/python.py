c323aef7e8ff380eeb03ede42d473a40f9800a16
Speriad <speria7755@gmail.com>
2021-08-01 21:18:45 +0900

    python

--- python/python.py
+++ python/python.py
@@ -188,3 +188,53 @@ print(students2)
 
 #F U N C T I O N
 
+def open_account():  
+    print('새로운 계좌가 생성됨')
+
+def deposit(balance,money):    #예금하기
+    print('입금이 완료 되었습니다. 잔액은 {0}원 입니다.'.format(balance+money))
+    return balance+money
+
+def withdraw(balance, money): #출금하기
+    if balance >= money:
+        print('출금이 완료 되었습니다. 잔액은 {0} 원입니다.'.format(balance-money))
+        return balance-money
+    else:
+        print('출금이 완료되지 않았습니다. 잔액은 {0}원 입니다.'.format(balance))
+        return balance
+
+def withdraw_night(balance,money):
+    commission = 100
+    return commission, balance - money - commission
+
+
+balance=0 #잔액
+balance = deposit(balance,1000)
+print(balance)
+
+commission, balance = withdraw_night(balance,500)
+print('수수료는 {0} 원이며, 잔액은 {1} 원입니다.'.format(commission, balance))
+
+#기본값
+
+# def profile(name, age, main_lang):
+#     print("dlfma : {0}\t나이 : {1}\t주 사용 언어 : {2}"\
+#         .format(name, age, main_lang))
+
+# profile('Drake',20,'파이썬')
+# profile('Rocky',25,'자바스크립트')
+
+def profile(name, age=17, main_lang='파이썬'):
+    print('이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}'\
+        .format(name, age, main_lang))
+
+profile('유재석')
+profile('김태호')
+
+#키워드값
+
+def profile2(name, age, main_lang):
+    print(name, age, main_lang)
+
+profile2(name='유재석',main_lang='파이썬',age=20)
+profile2(main_lang='자바',age=25,name='김태호')
