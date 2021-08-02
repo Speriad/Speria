4a231add459cb4202609dad8a0e6480c80fa379e
Speriad <speria7755@gmail.com>
2021-08-02 19:59:52 +0900

    python

--- python/python.py
+++ python/python.py
@@ -290,9 +290,6 @@ for subject, score in scores.items():
 for num in range(1,21):
     print('대기번호 : '+str(num).zfill(3))        #zfill = 0채우기
 
-answer = input('아무거나 입력 하셈 : ')
-print('님은 방금 '+answer+'이라고 말하셨음')
-
 #다양한 출력포멧
 
 print('{0:>10}'.format(500))    #빈자리는 빈공간으로 두고 오른쪽 정렬을 하고 10자리 확보
@@ -393,7 +390,8 @@ print('{0:.2f}'.format(5/3))    #소수점 둘째 자리까지 출력
 # print('체력 {0}, 공격력 {1}\n'.format(tank_hp, tank_damage))
 
 # def attack(name, location, damage):
-#     print('{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]'.format(name, location, damage))
+#     print('{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]'\
+# .format(name, location, damage))
 
 # attack(name,'1시',damage)
 # attack(tank_name,'1시',tank_damage)
@@ -407,15 +405,38 @@ class Unit:
         print('{0} 유닛이 생성 되었습니다'.format(self.name))
         print('체력 {0}, 공격력 {1}'.format(self.hp, self.damage))
 
-marine1 = Unit('마린',40,5)
-marine2 = Unit('마린',40,5)
-tank = Unit('탱크',150,35)
+# marine1 = Unit('마린',40,5)
+# marine2 = Unit('마린',40,5)
+# tank = Unit('탱크',150,35)
 
-wraith1 = Unit('레이스',80,5)
-print('유닛 이름 : {0}, 공격력 : {1}'.format(wraith1.name, wraith1.damage))
+# wraith1 = Unit('레이스',80,5)
+# print('유닛 이름 : {0}, 공격력 : {1}'.format(wraith1.name, wraith1.damage))
 
-wraith2 = Unit('레이스',80,5)
-wraith2.clocking = True
+# wraith2 = Unit('레이스',80,5)
+# wraith2.clocking = True
 
-if wraith2.clocking == True:
-    print('{0} 는 현재 클로킹 상태입니다.'.format(wraith2.name))
+# if wraith2.clocking == True:
+#     print('{0} 는 현재 클로킹 상태입니다.'.format(wraith2.name))
+
+class AttackUnit:
+    def __init__(self, name, hp, damage):
+        self.name = name  
+        self.hp = hp       
+        self.damage = damage  
+
+    def attack(self,location):
+        print('{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]'\
+            .format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print('{0} : {1} 데미지를 입었습니다.'.format(self.name, damage))
+        self.hp = self.hp - damage
+        print('{0} : 현재 체력은 {1} 입니다'.format(self.name, self.hp))
+        if self.hp <= 0 :
+            print('{0} : 파괴되었습니다.'.format(self.name))
+
+firebat1 = AttackUnit('파이어뱃', 50, 16)
+firebat1.attack('5시')
+
+firebat1.damaged(25)
+firebat1.damaged(25)
