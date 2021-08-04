947d7391a9b199d15f88c8272f57f640250e2b78
Speriad <speria7755@gmail.com>
2021-08-04 13:10:08 +0900

    python

--- python/python.py
+++ python/python.py
@@ -370,7 +370,7 @@ print('{0:.2f}'.format(5/3))    #소수점 둘째 자리까지 출력
 #     print(study_file.read())          #study_file 을 읽음
 
 #C L A S S
-
+from random import*
 # name='마린'
 # hp=40
 # damage=5
@@ -397,68 +397,150 @@ print('{0:.2f}'.format(5/3))    #소수점 둘째 자리까지 출력
 # attack(tank_name,'1시',tank_damage)
 # attack(tank2_name,'1시',tank2_damage)
 
-class Unit:
-    def __init__(self, name, hp, speed):
-        self.name = name   #멤버 변수
-        self.hp = hp     #멤버 변수
-        self.speed=speed       #멤버 변수
-        print('{0} 유닛이 생성되었습니다.'.format(name))
+# class Unit:
+#     def __init__(self, name, hp, speed):
+#         self.name = name   #멤버 변수
+#         self.hp = hp     #멤버 변수
+#         self.speed=speed       #멤버 변수
+#         print('{0} 유닛이 생성되었습니다.'.format(name))
 
     
-    def move(self, location):
-        print('[지상 유닛 이동]')
-        print('{0} : {1} 방향으로 이동합니다. [속도 {2}]'\
-            .format(self.name, location, self.speed))
+#     def move(self, location):
+#         print('[지상 유닛 이동]')
+#         print('{0} : {1} 방향으로 이동합니다. [속도 {2}]'\
+#             .format(self.name, location, self.speed))
 
-class AttackUnit(Unit):
-    def __init__(self, name, hp, speed, damage):
-        Unit.__init__(self, name, hp, speed)
-        self.damage = damage   
+# class AttackUnit(Unit):
+#     def __init__(self, name, hp, speed, damage):
+#         Unit.__init__(self, name, hp, speed)
+#         self.damage = damage   
 
-    def attack(self,location):
-        print('{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]'\
-            .format(self.name, location, self.damage))
+#     def attack(self,location):
+#         print('{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]'\
+#             .format(self.name, location, self.damage))
 
-    def damaged(self, damage):
-        print('{0} : {1} 데미지를 입었습니다.'.format(self.name, damage))
-        self.hp = self.hp - damage
-        print('{0} : 현재 체력은 {1} 입니다'.format(self.name, self.hp))
-        if self.hp <= 0 :
-            print('{0} : 파괴되었습니다.'.format(self.name))
+#     def damaged(self, damage):
+#         print('{0} : {1} 데미지를 입었습니다.'.format(self.name, damage))
+#         self.hp = self.hp - damage
+#         print('{0} : 현재 체력은 {1} 입니다'.format(self.name, self.hp))
+#         if self.hp <= 0 :
+#             print('{0} : 파괴되었습니다.'.format(self.name))
 
-firebat1 = AttackUnit('파이어뱃', 50, 16, 3)
-firebat1.attack('5시')
+# firebat1 = AttackUnit('파이어뱃', 50, 16, 3)
+# firebat1.attack('5시')
 
-firebat1.damaged(25)
-firebat1.damaged(25)
+# firebat1.damaged(25)
+# firebat1.damaged(25)
 
-class Flyable:     
-    def __init__(self, flying_speed):
-        self.flying_speed = flying_speed
+# class Flyable:     
+#     def __init__(self, flying_speed):
+#         self.flying_speed = flying_speed
 
-    def fly(self, name, location):       
-        print('{0} : {1} 방향으로 날아갑니다. [속도 {2}]'\
-            .format(name, location, self.flying_speed))
+#     def fly(self, name, location):       
+#         print('{0} : {1} 방향으로 날아갑니다. [속도 {2}]'\
+#             .format(name, location, self.flying_speed))
 
-class FlyableAttackUnit(AttackUnit, Flyable): 
-        def __init__(self, name, hp, damage, flying_speed):
-            AttackUnit.__init__(self, name, hp, 0, damage)
-            Flyable. __init__(self, name, hp, damage)
+# class FlyableAttackUnit(AttackUnit, Flyable): 
+#         def __init__(self, name, hp, damage, flying_speed):
+#             AttackUnit.__init__(self, name, hp, 0, damage)
+#             Flyable. __init__(self, name, hp, damage)
 
-        def move(self, location):
-            print('[공중 유닛 이동]')
-            self.fly(self.name, location)
+#         def move(self, location):
+#             print('[공중 유닛 이동]')
+#             self.fly(self.name, location)
 
-class Marine(AttackUnit):
-    def __init__(self):
-        AttackUnit.__init__(self, '마린', 40, 1, 5)
+# class Wraith(FlyableAttackUnit):
+#     def __init__(self):
+#         FlyableAttackUnit.__init__(self, '레이스', 80, 20, 5)
+#         self.clocked = False #처음에는 클로킹 모드가 아니다.
+        
+#     def clocking(self):
+#         if self.clocked == True:   #클로킹 모드이므로 해제하면됨
+#             print('{0} : 클로킹 모드 해제합니다.'.format(self.name))
+#             self.clocked = False
+#         else : 
+#             print('{0} : 클로킹 모드 설정합니다.'.format(self.name))
+#             self.clocked = True
 
-    def stimpack(self):
-        if self.hp > 10:
-            self.hp = self.hp-10
-            print('{0} : 스팀팩을 사용합니다. (HP 10 감소)'.format(self.name))
-        else:print('{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다.'.format(self.name))
+# class Marine(AttackUnit):
+#     def __init__(self):
+#         AttackUnit.__init__(self, '마린', 40, 1, 5)
 
-class Tank(AttackUnit):
-    def __init__(self):
-        AttackUnit.__init__(self, '마린', 40, 1, 5)
+#     def stimpack(self):
+#         if self.hp > 10:
+#             self.hp = self.hp-10
+#             print('{0} : 스팀팩을 사용합니다. (HP 10 감소)'.format(self.name))
+#         else:print('{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다.'.format(self.name))
+
+# class Tank(AttackUnit):
+#     seize_developed = False #시즈모드 개발여부
+
+#     def __init__(self):
+#         AttackUnit.__init__(self, '마린', 150, 1, 5)
+#         self.seize_mode = False
+
+#     def set_seize_mode(self):
+#         if Tank.seize_developed == False:
+#             return
+#         #시즈모드가 아닐때 -> 시즈모드를 키기
+#         if self.seize_mode == False:
+#             print('{0} : 시즈모드로 전환합니다.'.format(self.name))
+#             self.damage *= 2
+#             self.seize_mode = True
+#         #시즈모드 일때 -> 시즈모드를 끄기
+#         else:
+#             print('{0} : 시즈 모드를 해제합니다.'.format(self.name))
+#             self.damage /= 2
+#             self.seize_mode = False
+
+# def game_start():
+#     print('[알림] 새로운 게임을 시작합니다.')
+
+# def game_over():
+#     print('Player : GG')
+#     print('[Player] 님이 게임에서 퇴장하셨습니다.')
+
+
+# game_start()
+
+# m1 = Marine()
+# m2 = Marine()
+# m3 = Marine()
+
+# t1=Tank()
+# t2=Tank()
+
+# w1 = Wraith()
+
+# attack_units = []
+# attack_units.append(m1)
+# attack_units.append(m2)
+# attack_units.append(m3)
+# attack_units.append(t1)
+# attack_units.append(t2)
+# attack_units.append(w1)
+
+# for unit in attack_units:
+#     unit.move('1시')
+
+# #탱크 시즈모드 개발
+# Tank.seize_developed = True
+# print('[알림] 탱크시즈모드 개발이 완료 되었습니다.')
+
+# #공격모드 준비(탱크 시즈모드 레이스 클로킹)
+
+# for unit in attack_units:
+#     if isinstance(unit, Marine):
+#         unit.stimpack()
+#     elif isinstance(unit, Tank):
+#         unit.set_seize_mode()
+#     elif isinstance(unit, Wraith):
+#         unit.clocking()
+
+# for unit in attack_units:
+#     unit.attack('1시')
+
+# for unit in attack_units:
+#     unit.damaged(randint(5,21))  #공격은 5~ 20 사이로 받음
+
+# game_over()
