3241d07bd9fab63385ae2e97af0dcc232980042e
Speriad <speria7755@gmail.com>
2021-08-14 17:48:02 +0900

    python

--- python/main.py
+++ python/main.py
@@ -2,16 +2,25 @@ import pygame
 
 pygame.init()
 
-screen_width= 400
+screen_width= 480
 screen_height = 640
 pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption('Testing')
+
+background = pygame.image.load("C:\\무겸\\Programming\\Speria\\python\\blue.png")
 
 running=True
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+  
+  screen.blit(background, (0, 0))
+
+  pygame.display.update()
+  
+  
+pygame.display.update()
 
 pygame.quit()
