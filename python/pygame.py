98e371761d49003f27d28bc0eb5269899b5a74f9
Speriad <speria7755@gmail.com>
2021-08-14 16:52:20 +0900

    python

--- python/pygame.py
+++ python/pygame.py
@@ -1,17 +1,1 @@
 import pygame
-
-pygame.init()
-
-screen_width= 400
-screen_height = 640
-pygame.display.set_mode((screen_width, screen_height))
-
-pygame.display.set_caption('Testing')
-
-running=True
-while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-
-pygame.quit()
