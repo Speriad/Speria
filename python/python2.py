f45dd0372438201d5fd0a1b3c4df179c6a8b3400
Speria <speria7755@gmail.com>
2021-08-25 11:30:34 +0900

    python2 ayy

--- python/python2.py
+++ python/python2.py
@@ -1,1 +1,8 @@
-print('Hello World')
+def plus(num1,num2):
+    return int(num1)+int(num2)
+
+def multi(num1,num2):
+    return int(num1)*int(num2)
+
+print('{0} {1}'.format('The result is',multi(20,30)))
+print('{0} {1}'.format('The result is',plus(40,30)))
