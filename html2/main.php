01f2a2be6000ce396fb00d5dae9625ac48cfa3cd
Speriad <speria7755@gmail.com>
2021-09-01 13:09:48 +0900

    Bitnami --> XAMPP

---
+++ html2/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+   <head>
+       <title>PHP</title>
+       <meta charset="utf-8"/>
+       <link href="https://lh3.googleusercontent.com/kJhdRiXooM4L1CLW3EtC-okkwhPUrhFu6psYDJW17RMxCuNtXiQ-e3OfdmQ_m6ktyVmLKaP9KXEVIM2JHA=w544-h544-l90-rj" rel="shortcut icon" type="image/x-icon">
+   </head>
+   <body>
+        <?php
+            print('Hello World')
+        ?>
+   </body>
+<html>
