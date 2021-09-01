a5979ccdc2dcdcacd70c8afca2dbbddca4067fbd
Speriad <speria7755@gmail.com>
2021-09-01 15:03:33 +0900

    sDFJ;oisadhzof

--- html2/main.php
+++ html2/main.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
    <head>
-       <title>PHP</title>
+       <title>PHP (local)</title>
        <meta charset="utf-8"/>
        <link href="https://lh3.googleusercontent.com/kJhdRiXooM4L1CLW3EtC-okkwhPUrhFu6psYDJW17RMxCuNtXiQ-e3OfdmQ_m6ktyVmLKaP9KXEVIM2JHA=w544-h544-l90-rj" rel="shortcut icon" type="image/x-icon">
    </head>
@@ -11,4 +11,4 @@
             print('This is '.$d.' Yeah');   //문자.변수.문자 형태로 써야함
         ?>
    </body>
-<html>
+</html>
