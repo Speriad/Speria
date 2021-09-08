3d8bec85a84cfa6b1c651cd0521f080fa5be4c6e
Speriad <speria7755@gmail.com>
2021-09-09 00:38:35 +0900

    커밋

--- html2/etc.php
+++ html2/etc.php
@@ -48,6 +48,137 @@
     </div>
   </nav>
 
+  <section class='p-5'>
+    <div class='container'>
+      <div class='row text-center g-4'>
+        <div class='col-md'>
+          <div class='card bg-secondary text-light'>
+            <div class='card-body text-center'>
+              <div class='h1 mb-3'>
+              <i class="bi bi-chat-fill"></i>
+            </div>
+            <h3 class='card-title'>
+              Registration
+            </h3>
+            <p class='card-text'>
+              About Registration
+            </p>
+            <a href='#crm' class='btn btn-outline-warning'>Read More</a>
+            </div>
+          </div>
+        </div>
+        <div class='col-md'>
+          <div class='card bg-dark text-light'>
+            <div class='card-body text-center'>
+              <div class='h1 mb-3'>
+              <i class="bi bi-file-earmark-music-fill"></i>
+            </div>
+            <h3 class='card-title'>
+              Copyright
+            </h3>
+            <p class='card-text'>
+              About Type Beat's Copyright
+            </p>
+            <a href='#trm' class='btn btn-outline-warning'>Read More</a>
+            </div>
+          </div>
+        </div>
+        <div class='col-md'>
+          <div class='card bg-secondary text-light'>
+            <div class='card-body text-center'>
+              <div class='h1 mb-3'>
+              <i class="bi bi-justify-left"></i>
+            </div>
+            <h3 class='card-title'>
+              And etc..
+            </h3>
+            <p class='card-text'>
+              Everything About Moohipgal's Info
+            </p>
+            <a href='#lrm' class='btn btn-outline-warning'>Read More</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  </section>
+
+  
+  <section class='p-3'>
+  <div id='crm' class='container-fluid text-center bg-black text-white p-5'>
+    <h1 class='p-3'>About Registeration</h1>
+  <p>
+  <a class="btn btn-outline-warning" data-bs-toggle="collapse" href="#c1" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Why I have to do Registration?
+  </a>
+         
+  <a class="btn btn-outline-warning" data-bs-toggle="collapse" href="#c2" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Why I have to do Registration?
+  </a>
+</p>
+<div class="collapse" id="c1">
+  <div class="card card-body bg-dark">
+    Just Register
+  </div>
+</div>
+<div class="collapse bg-dark" id="c2">
+  <div class="card card-body bg-dark">
+    Just Do it
+  </div>
+</div>
+</div>
+      </section>
+
+      <section class='p-3'>
+<div id='trm' class='container-fluid text-center bg-black text-white p-5'>
+    <h1 class='p-3'>About Copyright</h1>
+  <p>
+  <a class="btn btn-outline-warning" data-bs-toggle="collapse" href="#c3" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Why I have to do sdf
+  </a>
+         
+  <a class="btn btn-outline-warning" data-bs-toggle="collapse" href="#c4" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Why I have to do asd
+  </a>
+</p>
+<div class="collapse" id="c3">
+  <div class="card card-body bg-dark">
+    Hello
+  </div>
+</div>
+<div class="collapse bg-dark" id="c4">
+  <div class="card card-body bg-dark">
+    World
+  </div>
+</div>
+</div>
+      </section>
+
+      <section class='p-3'>
+<div id='lrm' class='container-fluid text-center bg-black text-white p-5'>
+    <h1 class='p-3'>About Everything</h1>
+  <p>
+  <a class="btn btn-outline-warning" data-bs-toggle="collapse" href="#c5" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Drako
+  </a>
+         
+  <a class="btn btn-outline-warning" data-bs-toggle="collapse" href="#c6" role="button" aria-expanded="false" aria-controls="collapseExample">
+    Young Thug
+  </a>
+</p>
+<div class="collapse" id="c5">
+  <div class="card card-body bg-dark">
+    HiHi
+  </div>
+</div>
+<div class="collapse bg-dark" id="c6">
+  <div class="card card-body bg-dark">
+    I love Kendrick lamar
+  </div>
+</div>
+</div>
+      </section>
+
     
     
   
