577795757ccfe7ace3978889a13e37af6874bbcb
Speriad <speria7755@gmail.com>
2021-09-08 15:28:02 +0900

    ATL Jacob AT ATL Jacob

---
+++ html2/lyrics.php
@@ -0,0 +1,72 @@
+<!doctype html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="https://i.ibb.co/hKp3HtP/DAMN.jpg" rel="shortcut icon" type="image/x-icon">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+	 crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+	<title>MooHipGal</title>
+	<style type='text/css'>
+		body {
+			background-color: white;
+		}
+        a{
+            text-decoration: none;
+            color:white;
+        }
+	</style>
+</head>
+
+<body>
+	<!-- NAVBAR 구현 -->
+	<nav class='navbar navbar-expand-lg bg-dark navbar-dark py-3'>
+		<div class='container'>
+			<a href='lyrics.php' class='navbar-brand'>Lyrics</a>
+			<button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navmenu'><span class='navbar-toggler-icon'></span></button>
+      <div class='collapse navbar-collapse' id='navmenu'>
+        <ul class='navbar-nav ms-auto'>
+          <li class='nav-item'>
+            <a href='community.php' class='nav-link'>Community</a>
+          </li>
+          <li class='nav-item'>
+            <a href='typebeat.php' class='nav-link'>Type Beat</a>
+          </li>
+          <li class='nav-item'>
+            <a href='albums.php' class='nav-link'>Albums</a>
+          </li>
+          <li class='nav-item'>
+            <a href='index.php' class='nav-link'>Lobby Page</a>
+          </li>
+          <li class='nav-item'>
+            <a href='etc.php' class='nav-link'>etc.</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+    
+    
+  
+
+
+
+  
+
+
+
+
+
+
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+	 crossorigin="anonymous">
+	</script>
+</body>
+
+</html>
