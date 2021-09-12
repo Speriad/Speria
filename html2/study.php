a09c6fb2d68919d96b4f427f3b61c9c9c98cc296
Speriad <speria7755@gmail.com>
2021-09-12 23:33:37 +0900

    Jigga

---
+++ html2/study.php
@@ -0,0 +1,41 @@
+<!doctype html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_php_icon_130266.png" rel="shortcut icon" type="image/x-icon">
+	<title>Study PHP</title>
+</head>
+<body>
+
+
+    
+    
+<?php
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+  
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
