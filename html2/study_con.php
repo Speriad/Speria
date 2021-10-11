4a536365fead59009cba8db87062e615ce18f9c4
Speriad <speria7755@gmail.com>
2021-10-12 00:38:19 +0900

    Hello World

--- html2/study_con.php
+++ html2/study_con.php
@@ -1,6 +1,13 @@
 <?php
-mysqli_connect('localhost','root','','test');
+$conn = mysqli_connect('localhost','root','','test');
 
+$query = 'INSERT into registration(nickname,say) 
+VALUE ("Testing","Hello World!")';
+
+$result = mysqli_query($conn,$query);
+if($result === false){
+    echo mysqil_error($conn);
+}
 
 
 
