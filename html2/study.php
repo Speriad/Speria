fd451c62acd7985c177e75021f7acfe3e7bf0a02
Speriad <speria7755@gmail.com>
2021-10-12 00:17:51 +0900

    FUnk!

--- html2/study.php
+++ html2/study.php
@@ -4,6 +4,15 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link href="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_php_icon_130266.png" rel="shortcut icon" type="image/x-icon">
 	<title>Study</title>
+  <style>
+    body{
+      background-color: black;
+      color: white;
+    }
+    a{
+      color: yellow;
+    }
+  </style>
 </head>
 <body>
 <h1>Parameter</h1>
