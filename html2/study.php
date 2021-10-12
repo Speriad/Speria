caa7ffbed5a391b3bce1ea5c06f98cd264433d5c
Speriad <speria7755@gmail.com>
2021-10-13 00:24:10 +0900

    COMMIT

--- html2/study.php
+++ html2/study.php
@@ -1,3 +1,6 @@
+<?php
+$conn = mysqli_connect('localhost','root','','test');
+?>
 <!doctype html>
 
 <head>
@@ -24,6 +27,19 @@
 <br><br><button role='submit' class='btn btn-outline-warning'>올리기</button> 
 </form>
 
+<h1>WEB</h1>
+<ol>
+    <?php
+// DB 연결후, 셀렉트, 쿼리, 로우지정 순서대로 간다
+// mysqli fetch array 함수는 컬럼을 하나씩 리턴 해주는 함수임
+// [0] = id, [1] = nickname, [2] = say 이며
+$sql = 'SELECT * FROM registration';
+$result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($result)){
+  echo "<li>{$row['say']}</li>";
+};
+    ?>
+</ol>
 
 
 
