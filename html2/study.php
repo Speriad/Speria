73c73e645ede9eb1b963b7ca1735a9a85c614b90
Speriad <speria7755@gmail.com>
2021-10-12 19:37:18 +0900

    w

--- html2/study.php
+++ html2/study.php
@@ -2,6 +2,8 @@
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+	 crossorigin="anonymous">
     <link href="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_php_icon_130266.png" rel="shortcut icon" type="image/x-icon">
 	<title>Study</title>
   <style>
@@ -19,7 +21,7 @@
 <form action='study_con.php' method='post'>
 <input type='text' name = 'nickname' placeholder='TITLE'>
 <br><br><textarea name='say' placeholder='Say Something..'></textarea>
-<br><br><input type='submit'> 
+<br><br><button role='submit' class='btn btn-outline-warning'>올리기</button> 
 </form>
 
 
@@ -29,7 +31,9 @@
 
 
 
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+	 crossorigin="anonymous">
+	</script>
 </body>
 
 </html>
--- html2/study_con.php
+++ html2/study_con.php
@@ -1,19 +1,31 @@
-<title>MySQL</title>
+<!doctype html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+	 crossorigin="anonymous">
+    <link href="https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_php_icon_130266.png" rel="shortcut icon" type="image/x-icon">
+	<title>MySQL</title>
+  <style>
+    body{
+      background-color: black;
+      color: white;
+    }
+    a{
+      color: yellow;
+    }
+  </style>
+</head>
+<body>
+
+
 <?php
 $conn = mysqli_connect('localhost','root','','test');
-
-$title = $_POST['nickname'];
-$saysomething = $_POST['say'];
 
 $puttodb = "INSERT into registration(nickname,say)
 VALUE ( '{$_POST['nickname']}' , '{$_POST['say']}' )";
 
-
 mysqli_query($conn,$puttodb);
-
-echo $title.'<br>';
-echo '<br>'.$saysomething;
-echo '<br>'.$puttodb
 
 
 
@@ -44,3 +56,8 @@ echo '<br>'.$puttodb
 
 
 ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+	 crossorigin="anonymous">
+	</script>
+</body>
+</html>
