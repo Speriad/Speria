a4496f4f79fff14d3daa2580626dd630ef121bd6
Speriad <speria7755@gmail.com>
2021-10-16 14:37:36 +0900

    Wake up Mr. WEST!!

--- html2/connvar.php
+++ html2/connvar.php
@@ -1,5 +1,6 @@
+<?php
 $conn = mysqli_connect('localhost','root','','test');
 mysqli_set_charset($conn,'utf8');
-
+?>
 <!-- $conn = mysqli_connect('sql206.epizy.com','epiz_29599569','8DfkA57d5o77A','epiz_29599569_php'); -->
 <!-- $conn = mysqli_connect('localhost','root','','test'); -->
