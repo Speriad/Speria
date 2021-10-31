2713ccad370744deaa7496315d125e19f8d13f9b
Speriad <speria7755@gmail.com>
2021-10-31 12:23:32 +0900

    1616516189

--- html2/index.php
+++ html2/index.php
@@ -143,10 +143,10 @@
               <i class="bi bi-justify-left"></i>
             </div>
             <h3 class='card-title'>
-              가사
+              뉴스
             </h3>
             <p class='card-text'>
-              여러분이 가장 좋아하는 노래의 가사를 찾아보세요!
+              외국힙합 뉴스를 누구보다 빠르게 접해보세요
             </p>
             <a href='#lrm' class='btn btn-outline-warning'>더 알아보기</a>
             </div>
@@ -162,7 +162,7 @@
     <div class='container'>
       <div class='row align-items-center justify-content-center'>
       <div class='col-md'>
-          <img src='https://i.pinimg.com/originals/01/1b/24/011b24f9cb707b842aeb862ef9d54e5a.gif' class='img-fluid' alt=''>
+          <img src='https://cityofgood.sg/wp-content/uploads/2020/11/Success-3-2048x816.jpg' class='img-fluid' alt=''>
         </div>
         <div class='col-md p-5'>
           <h2>당신과 같이 힙합을 좋아하는 사람들과 모이세요!</h2>
@@ -209,15 +209,15 @@
           <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_B_fuU2qhoWCCgblAuozsKAdVZashFLfbinvp1GuZ-j9gGR2xY9NXDPHM1l7vfPQygjc&usqp=CAU' class='img-fluid' alt=''>
         </div>
         <div class='col-md p-5 text-black'>
-          <h2>당신이 가장 좋아하는 래퍼의 가사를 찾아보세요!</h2>
+          <h2>최신 외국힙합에 관한 소식을 접해보세요!</h2>
           <p class='lead text-black'>
-           언제나 당신이 좋아하는 곡의 가사를 찾을수 있습니다. 가사를 보며 노래를 따라 불러보기도 하세요!
+           외국 힙합에 관한 소식을 가장 빨리 접해보세요!
           </p>
           <p class='lead text-black'>
-            가사 수정 또는 만드는것을 제안 하는 기능도 만들어 질것입니다.
+            매일 매일 업데이트 됩니다.
           </p>
-          <a href='lyrics.php' class='btn btn-dark mt-3'>
-            <i class='bi bi-chevron-right'>가사로 들어가기</i>
+          <a href='news.php' class='btn btn-dark mt-3'>
+            <i class='bi bi-chevron-right'>뉴스로 들어가기</i>
           </a>
         </div>
       </div>
@@ -231,42 +231,37 @@
       <div class="accordion" id="questions">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button btn-warning collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-        What Should I do to join Community?
+      <button class="accordion-button btn-secondary collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        커뮤니티에 글은 어떻게 쓰나요?
       </button>
     </h2>
     <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>You can join Community anytime.</strong><br><br>Log-In System will be given to you. But you can join community by Log-Out User. But you can't write comment or post in Community.
-        But Accessing Community, Reading Other Community Members' Post are not needed Log-in. You can Login at the top of MooHipGal page. If you are in Mobile-Environment, Touch Hamberger-Button and 
-        Go to Login Page and You can Register very easily. Thanks for enjoying our community.
+        <strong>로그인이 필요합니다</strong><br><br>로그인을 한 후 커뮤니티로 들어가 글쓰기 기능을 이용하면 됩니다
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button btn-warning collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Does type beat cost money to use?
+      <button class="accordion-button btn-secondary collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        타입비트를 사용하려면 돈을 내야하나요?
       </button>
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>It doesn't cost for using.</strong><br><br>Moohipgal's Type beat is for Beginners' beat and Users must upload their beat for free. You can download other producers' Type-Beat any time. 
-        And For producers, You must agree the terms of type-beat's No-Copyright. And do not require users to give you money. And If you want to download producers' Type-Beat You must be log-in to Moohipgal.
-        Use real Producers' Type Beat anytime!
+        <strong>사용하는데 돈이 들지 않습니다</strong><br><br>무힙갤에 올리는 모든 비트는 무료 비트로 제공해야 하고 비트를 사용하는 사람은 무료로 사용할 수 있습니다
       </div>
     </div>
   </div>
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingThree">
-      <button class="accordion-button btn-warning collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        How can I report error in Moohipgal?
+      <button class="accordion-button btn-secondary collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        무힙갤에 정보 수정 기능이 있나요?
       </button>
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>You can send Report at Lobby Page</strong><br><br>You can report your discomfort anytime you want. Report is needed Log-In So you must be log-in before you report to developer. You must report 
-        error, advice, etc. But if you report not-associated things. You can be punished without warning.
+        <strong>로비 페이지에서 정보를 수정할 수 있습니다</strong><br><br>닉네임과 비밀번호를 수정할수 있으며 곧 이메일과 자세한 회원정보를 추가할 것 입니다
       </div>
     </div>
   </div>
@@ -274,44 +269,16 @@
 <div class="accordion" id="questions">
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingFour">
-      <button class="accordion-button btn-warning collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-      What is the difference of 'Albums' Page and 'Community' Page?
+      <button class="accordion-button btn-secondary collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+      뉴스 에서는 뭘 볼수 있나요?
       </button>
     </h2>
     <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>It is difference of Share and Information</strong><br><br>Community is for everything like sharing songs, advertising their music, or music video and also Albums! But 'Albums'
-        Page is a series of every album. You can find your albums' information written by other users. It is similar with WikiPedia. But it's for Hip-Hop Albums like Illmatic And To Pimp A Butterfly etc. 
-        If you want information of your favorite albums, You can visit 'Albums' Page and read that!
+        <strong>외국 힙합과 관련된 내용을 볼수 있습니다</strong><br><br>무힙갤에서 외국 힙합의 최신소식을 접해보세요
       </div>
     </div>
   </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingFive">
-      <button class="accordion-button btn-warning collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-        What's the 'Lyrics' Page and What can I get through it?
-      </button>
-    </h2>
-    <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>You can get your favorite songs' Lyrics Anytime!</strong><br><br>If you want to get your favorite songs' Lyrics, You must visit 'Lyrics' Page! Lyrics Page is Uploaded by other users.
-        And finally, If Songs' Lyrics have errors, Report to developer in Lobby Page. Thanks.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingSix">
-      <button class="accordion-button btn-warning collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-        Does MooHipGal have Mebership service or Subscribing?
-      </button>
-    </h2>
-    <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>No, There isn't. And We don't have an idea like Money-Needed Service yet</strong><br><br>
-      </div>
-    </div>
-  </div>
-</div>
     </div>
   </section>
 
