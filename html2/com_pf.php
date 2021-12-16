464f62ee046205406466a0fc0243be6bdeea6451
Speriad <speria7755@gmail.com>
2021-12-16 19:56:08 +0900

    dd

--- html2/com_pf.php
+++ html2/com_pf.php
@@ -3,7 +3,9 @@
 <?php 
 include 'connvar.php';
 $update = "UPDATE registration set nickname='{$_POST['new_nickname']}' , pw='{$_POST['new_pw']}' where nickname='{$_SESSION['nickname']}'";
+$update2 = "UPDATE com set written='{$_POST['new_nickname']}' where written='{$_SESSION['nickname']}'"
 mysqli_query($conn, $update);
+mysqli_query($conn, $update2);
 $_SESSION['nickname'] = null;
 echo "<script>alert('다시 로그인해주세요')</script>";
 ?>
