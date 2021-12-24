1861a2e47dec786baa66103e5df0265fa79b07c9
Speriad <speria7755@gmail.com>
2021-12-24 23:32:52 +0900

    ㅇ

--- python/python2.py
+++ python/python2.py
@@ -5,10 +5,16 @@ pygame.init()
 size = [1200 , 800]
 screen = pygame.display.set_mode(size)
 
+white = (255,255,255)
+screen.fill(white)
+
 title = "Eunsoo's KeumChun Adventure"
 pygame.display.set_caption(title)
 
 clock = pygame.time.Clock()
+ss = pygame.image.load('C:/무겸/Programming/Speria/python/ak.PNG').convert_alpha()
+ss = pygame.transform.scale(ss, (88.665, 250))   #ss의 크기 변경
+screen.blit(ss, (550, 550))     #스크린에 ss를 (x, y)에 표시
 
 SB = 0
 while SB == 0:
@@ -19,3 +25,4 @@ while SB == 0:
     pygame.display.flip() #업데이트하기 (안쓰면 저장안됨)
 
 pygame.quit()
+
