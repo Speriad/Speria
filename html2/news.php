46d9898bd5c9b9b1b47847a47c9572546992c4ac
Speriad <speria7755@gmail.com>
2022-02-17 13:03:36 +0900

    Hello WOrld

--- html2/news.php
+++ html2/news.php
@@ -17,24 +17,14 @@
 	<!-- NAVBAR 구현 -->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3">
   <div class="container">
-    <a class="navbar-brand">오늘의 힙합 (뉴스)</a>
+    <a class="navbar-brand">뉴스</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#content" aria-controls="content" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="content">   
-      <?php
-	  if(isset($_POST['nickname'])){
-        $button1 = "<form action='index.php' method='post' class='d-flex p-2'><input type='hidden' name='nickname' value='{$_POST['nickname']}'>
+      	<form action='index.php' class='d-flex p-2'>
         <button class='btn btn-outline-primary' type='submit'>로비 페이지로 돌아가기</button>
-        </form>";
-        echo $button1;
-	  }else{
-		$button1 = "<form action='index.php' class='d-flex p-2'>
-        <button class='btn btn-outline-primary' type='submit'>로비 페이지로 돌아가기</button>
-        </form>";
-        echo $button1;
-	  };
-      ?>
+        </form>
   </div>
   </div>
 </nav>
@@ -43,15 +33,15 @@
     <div class='container'>
       <div class='row align-items-center justify-content-center'>
       <div class='col-md'>
-          <img src='https://is3-ssl.mzstatic.com/image/thumb/Features115/v4/13/3d/89/133d89fd-9305-6eae-14ad-1668e0e5279d/mza_4449571582288811102.png/800x800cc.jpg' class='img-fluid' alt=''>
+          <a href='https://www.youtube.com/channel/UCqf-kTp9ERV5T1rPayno7LA'><img src='https://w.namu.la/s/6db1d90fa623d8b54f4e2c93877b35a42ef64039030d2eb6432b6f9d8d007b18ab23af987134de8f3cec1a36206c69bbe977e0ec74b3d1fe646767bb1ca8e07660c15a04795ec2f6e679aba91515ede6fa4bf2fafd4e0bc869ed8b22fd2e5d99' class='img-fluid' alt=''></a>
         </div>
         <div class='col-md p-5'>
           <h1>오늘의 아티스트</h1>
           <h2 class='lead'>
-            켄드릭 라마
+            프랭크 오션
           </h2>
           <br><p class='lead'>
-            가장 위대한 래퍼, 켄드릭 라마
+            가장 위대한 아티스트, 프랭크 오션
           </p>
         </div>
       </div>
@@ -61,15 +51,15 @@
     <div class='container'>
       <div class='row align-items-center justify-content-center'>
       <div class='col-md'>
-          <img src='https://images-na.ssl-images-amazon.com/images/I/713N4ZIYsEL.jpg' class='img-fluid' alt=''>
+          <a href='https://www.youtube.com/playlist?list=PLzoqV_VvWIwGzYTcm3r1JwqgQOBXTvKyd'><img src='https://lh3.googleusercontent.com/TWBi2M7D8gIwoo3NmhGfoVKI-PuzDunLVYpmLCbeP8Uw2YWpnjttlxmVvpVaO8uSjmLPjHgy6iGXxlPF=w544-h544-l90-rj' class='img-fluid' alt=''></a>
         </div>
         <div class='col-md p-5 text-white'>
           <h1>오늘의 앨범</h1>
           <h2 class='lead'>
-           My Beautiful Dark Twisted Fantasy by 칸예 웨스트
+          Blonde by 프랭크 오션
           </h2>
           <br><p class='lead'>
-            가장 위대한 앨범, My Beautiful Dark Twisted Fantasy by 칸예 웨스트
+            가장 위대한 앨범, Blonde by 프랭크 오션
           </p>
         </div>
       </div>
@@ -79,15 +69,15 @@
     <div class='container'>
       <div class='row align-items-center justify-content-center'>
       <div class='col-md'>
-          <img src='https://ww.namu.la/s/2d598f934357dc6b034633bbf49e3662d5642909a0721abd1fb2b417520dd37e6b1c209dff47b84e5d32ea01194f27b1bc61d980f20650e8b724ad4a57940b78957cc43963860cf6d41c42926405433510bc6db034ece2ae04fe7cdb236f7d09' class='img-fluid' alt=''>
+          <a href='https://www.youtube.com/watch?v=q2F4IqbszEg'><img src='https://lh3.googleusercontent.com/QqPmw-qkbfqfrgM15OIA4t8KVkaKmxUxgEkpI4ZdfPjTkvpgjv8tJrCa5sfQed2XqsZUXjTes2rpX7Su-Q=w544-h544-l90-rj' class='img-fluid' alt=''></a>
         </div>
         <div class='col-md p-5'>
           <h1>오늘의 곡</h1>
           <h2 class='lead'>
-            Heartless by 칸예 웨스트
+            HANDSOMER by 러스
           </h2>
           <br><p class='lead'>
-            가장 위대한 곡, Heartless by 칸예 웨스트
+            가장 위대한 곡, HANDSOMER by 러스
           </p>
         </div>
       </div>
