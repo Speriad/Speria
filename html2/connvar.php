f55e0f971e9553d2ac2ef3700b81b48dec058105
Speriad <speria7755@gmail.com>
2022-02-25 22:44:53 +0900

    ㅇ

--- html2/connvar.php
+++ html2/connvar.php
@@ -5,17 +5,18 @@ $conn = mysqli_connect('localhost','root','','test');
 mysqli_set_charset($conn,'utf8');
 session_start();
 $head = "<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-<link href='https://i.ibb.co/hKp3HtP/DAMN.jpg' rel='shortcut icon' type='image/x-icon'>
+<link href='https://lh3.googleusercontent.com/0yu8Hk3siMPLzbkwOdGqgFpHlIl8ySOz-Ccj4k9KCuFf0HIF2q8xdjp_jCmOb3WoHk1tawPs4q0bjmnwGQ=w544-h544-l90-rj' rel='shortcut icon' type='image/x-icon'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We'
  crossorigin='anonymous'>
 <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css'>
-<meta property='og:description' content='The most popular and practical HipHop Community 'Moohipgal''>
-<meta property='og:image' content='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8wtA8KFXwKZ5hxyQC5ExHZexGmpwU-0m_Xw&usqp=CAU'>
+<meta property='og:title' content='무힙갤'>
+<meta property='og:description' content='The Greatest HipHop Community'>
+<meta property='og:image' content='https://lh3.googleusercontent.com/TWBi2M7D8gIwoo3NmhGfoVKI-PuzDunLVYpmLCbeP8Uw2YWpnjttlxmVvpVaO8uSjmLPjHgy6iGXxlPF=w544-h544-l90-rj'>
 <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js' integrity='sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj'
 	 crossorigin='anonymous'>
 	</script>
-<title>MooHipGal</title>
+<title>무힙갤ㄹ</title>
 <style>a{text-decoration:none;}</style>";
 echo $head;
 
