714492f4605a2dba1f1bc9dc3c3a6600677935ab
speria <speria7755@gmail.com>
2022-09-09 14:03:47 +0900

    maybe i can do dat

--- imageuploadtest/index.php
+++ imageuploadtest/index.php
@@ -1,48 +1,40 @@
 <?php
-error_reporting(0);
+// Create database connection
+$db = mysqli_connect("localhost", "root", "", "image_upload");
 
+// Initialize message variable
 $msg = "";
 
 // If upload button is clicked ...
 if (isset($_POST['upload'])) {
-    $filename = $_FILES["uploadfile"]["name"];
-    $tempname = $_FILES["uploadfile"]["tmp_name"];
-    $folder = "./image/" . $filename;
+    // Get image name
+    $image = $_FILES['image']['name'];
+    // Get text
+    $image_text = mysqli_real_escape_string($db, $_POST['image_text']);
 
-    $db = mysqli_connect("localhost", "root", "", "test");
+    // image file directory
+    $target = "images/" . basename($image);
 
-    // Get all the submitted data from the form
-    $sql = "INSERT INTO image (filename) VALUES ('$filename')";
-
-    // Execute query
+    $sql = "INSERT INTO images (image, image_text) VALUES ('$image', '$image_text')";
+    // execute query
     mysqli_query($db, $sql);
 
-    // Now let's move the uploaded image into the folder: image
-    if (move_uploaded_file($tempname, $folder)) {
-        echo "<h3> Image uploaded successfully!</h3>";
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+        $msg = "Image uploaded successfully";
     } else {
-        echo "<h3> Failed to upload image!</h3>";
+        $msg = "Failed to upload image";
     }
 }
+$result = mysqli_query($db, "SELECT * FROM images");
 ?>
-
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>Image Upload</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
+    <style type="text/css">
         #content {
             width: 50%;
-            justify-content: center;
-            align-items: center;
             margin: 20px auto;
             border: 1px solid #cbcbcb;
         }
@@ -52,44 +44,54 @@ if (isset($_POST['upload'])) {
             margin: 20px auto;
         }
 
-        #display-image {
-            width: 100%;
-            justify-content: center;
+        form div {
+            margin-top: 5px;
+        }
+
+        #img_div {
+            width: 80%;
             padding: 5px;
-            margin: 15px;
+            margin: 15px auto;
+            border: 1px solid #cbcbcb;
+        }
+
+        #img_div:after {
+            content: "";
+            display: block;
+            clear: both;
         }
 
         img {
+            float: left;
             margin: 5px;
-            width: 350px;
-            height: 250px;
+            width: 300px;
+            height: 140px;
         }
     </style>
 </head>
 
 <body>
     <div id="content">
-        <form method="POST" action="#" enctype="multipart/form-data">
-            <div class="form-group">
-                <input class="form-control" type="file" name="uploadfile" value="" />
-            </div>
-            <div class="form-group">
-                <button class="btn btn-primary" type="submit" name="upload">UPLOAD</button>
-            </div>
-        </form>
-    </div>
-    <div id="display-image">
         <?php
-        $query = " select * from image ";
-        $result = mysqli_query($db, $query);
-
-        while ($data = mysqli_fetch_assoc($result)) {
-        ?>
-            <img src="./image/<?php echo $data['filename']; ?>">
-
-        <?php
+        while ($row = mysqli_fetch_array($result)) {
+            echo "<div id='img_div'>";
+            echo "<img src='images/" . $row['image'] . "' >";
+            echo "<p>" . $row['image_text'] . "</p>";
+            echo "</div>";
         }
         ?>
+        <form method="POST" action="index.php" enctype="multipart/form-data">
+            <input type="hidden" name="size" value="1000000">
+            <div>
+                <input type="file" name="image">
+            </div>
+            <div>
+                <textarea id="text" cols="40" rows="4" name="image_text" placeholder="Say something about this image..."></textarea>
+            </div>
+            <div>
+                <button type="submit" name="upload">POST</button>
+            </div>
+        </form>
     </div>
 </body>
 
