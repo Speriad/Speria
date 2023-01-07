fd294f2e213bb7f5e457c959cc9815f3a1ca3a13
Speriad <speria7755@gmail.com>
2023-01-08 01:12:24 +0900

    why I cant commit workspace.xml

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -81,7 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
+          // children horizontally, and tries to be as tall as its parent
           //
           // Invoke "debug painting" (press "p" in the console, choose the
           // "Toggle Debug Paint" action from the Flutter Inspector in Android
